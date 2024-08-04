983da5167e87a69004174c29853e79d1823b4f6f
Bethwel123 <bethwellangat654@gmail.com>
2024-08-04 05:09:59 +0300

    Used state and events dynamically

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, setTasks }) {
+  const handleDelete = (taskToDelete) => {
+    const updatedTasks = tasks.filter((task) => task.text !== taskToDelete);
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task) => (
+        <Task
+          key={task.text}
+          text={task.text}
+          category={task.category}
+          onDelete={handleDelete}
+        />
+      ))}
     </div>
   );
 }
